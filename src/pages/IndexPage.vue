6dde0621162ba1f4ca5fa76344be7bf0e6085d11
Paradon Srita <paradonsrita2003@gmail.com>
2023-12-15 10:29:03 +0700

    Change your age from english to chinese-lang

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -19,10 +19,10 @@
         filled
         type="number"
         v-model="age"
-        label="Your age *"
+        label="你的年齡 *"
         lazy-rules
         :rules="[
-          val => val !== null && val !== '' || 'Please type your age',
+          val => val !== null && val !== '' || '請輸入您的年齡',
           val => val > 0 && val < 100 || 'Please type a real age'
         ]"
       />
